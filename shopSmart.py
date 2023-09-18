1bf79ea362f50a12532e1771e434649a1d8fe7b5
CaoDuc11 <caoduc2211@gmail.com>
2023-09-18 11:34:02 +0700

    21020303 Cao Trọng Đức

--- shopSmart.py
+++ shopSmart.py
@@ -30,8 +30,16 @@ def shopSmart(orderList, fruitShops):
         fruitShops: List of FruitShops
     """
     "*** YOUR CODE HERE ***"
-    return None
-
+    bestShop = fruitShops[0]		# initialize
+    lowestCost = bestShop.getPriceOfOrder(orderList)
+    
+    for shop in fruitShops[1:]:
+    	cost = shop.getPriceOfOrder(orderList)
+    	if cost < lowestCost:
+    		bestShop = shop
+    		lowestCost = cost
+    
+    return bestShop
 
 if __name__ == '__main__':
     "This code runs when you invoke the script from the command line"
